f2e27f2430ef15de8039def7b9792da84ac2f814
EdisonLeeeee <cnljt@outlook.com>
2020-06-03 12:02:24 +0800

    fix normalize_edge bugs for CPU

--- graphgallery/utils/tensor_utils.py
+++ graphgallery/utils/tensor_utils.py
@@ -4,10 +4,11 @@ import scipy.sparse as sp
 
 from numbers import Number
 from tensorflow.keras import backend as K
+from graphgallery import config
 
 
-def normalize_adj_tensor(adj, rate=-0.5, self_loop=True):
-    if self_loop:
+def normalize_adj_tensor(adj, rate=-0.5, add_self_loop=True):
+    if add_self_loop:
         adj = adj + tf.eye(adj.shape[0])
     row_sum = tf.reduce_sum(adj, 1)
     d_inv_sqrt = tf.pow(row_sum, rate)
@@ -20,10 +21,11 @@ def add_self_loop_edge(edge_index, edge_weight, n_nodes=None, fill_weight=1.0):
     if n_nodes is None:
         n_nodes = tf.reduce_max(edge_index)
 
-    diagnal_edge_index = np.transpose(np.diag_indices(n_nodes)).astype('int64')
+    range_arr = tf.range(n_nodes, dtype=config.intx())
+    diagnal_edge_index = tf.stack([range_arr, range_arr], axis=1)
     updated_edge_index = tf.concat([edge_index, diagnal_edge_index], axis=0)
 
-    diagnal_edge_weight = tf.zeros([n_nodes], dtype=self.floatx) + fill_weight
+    diagnal_edge_weight = tf.zeros([n_nodes], dtype=config.floatx()) + fill_weight
     updated_edge_weight = tf.concat([edge_weight, diagnal_edge_weight], axis=0)
 
     return updated_edge_index, updated_edge_weight
@@ -32,15 +34,15 @@ def add_self_loop_edge(edge_index, edge_weight, n_nodes=None, fill_weight=1.0):
 def normalize_edge(edge_index, edge_weight=None, n_nodes=None, fill_weight=1.0, rate=-0.5):
 
     if edge_weight is None:
-        edge_weight = tf.ones([edge_index.shape[0]], dtype=self.floatx)
+        edge_weight = tf.ones([edge_index.shape[0]], dtype=config.floatx())
 
     if n_nodes is None:
         n_nodes = tf.reduce_max(edge_index)
-
+        
     edge_index, edge_weight = add_self_loop_edge(edge_index, edge_weight, n_nodes=n_nodes, fill_weight=fill_weight)
 
     row, col = tf.unstack(edge_index, axis=1)
-    deg = tf.math.unsorted_segment_sum(edge_weight, row, num_segments=n_nodes)
+    deg = tf.math.unsorted_segment_sum(edge_weight, row, num_segments=n_nodes+1)
     deg_inv_sqrt = tf.pow(deg, rate)
 
     # check if exists NAN
