c89ae39a8144e67363dcfcba9c226e32ddbd628e
EdisonLeeeee <cnljt@outlook.com>
2020-07-28 21:13:56 +0800

    update to 0.1.5

--- graphgallery/__init__.py
+++ graphgallery/__init__.py
@@ -7,12 +7,14 @@ from graphgallery.utils.conversion import (check_and_convert, sparse_adj_to_edge
 
 from graphgallery.utils.data_utils import normalize_adj, normalize_x, Bunch, sample_mask
 from graphgallery.utils.shape_utils import repeat
+from graphgallery.utils.tqdm import tqdm
+
 
 from graphgallery import nn
 from graphgallery import utils
 from graphgallery import sequence
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __all__ = ['graphgallery', 'nn', 'utils', 'sequence', '__version__']
