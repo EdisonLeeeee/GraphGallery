958514abfe74bd908a906efcc6182042ec479537
EdisonLeeeee <cnljt@outlook.com>
2020-09-20 11:39:18 -0400

    fix save.py

--- graphgallery/utils/save.py
+++ graphgallery/utils/save.py
@@ -6,100 +6,101 @@ import tensorflow as tf
 _TORCH_POSTFIX = '.pt'
 _TF_POSTFIX = '.h5'
 
-def save_tf_model(model, file_path, overwrite=True, save_format=None, **kwargs):    
+
+def save_tf_model(model, filepath, overwrite=True, save_format=None, **kwargs):
 
     postfix = _TF_POSTFIX
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
 
-    model.save(file_path_with_postfix, overwrite=overwrite, save_format=save_format, **kwargs)
+    model.save(filepath_with_postfix, overwrite=overwrite, save_format=save_format, **kwargs)
 
 
-def save_torch_model(model, file_path, overwrite=True, save_format=None, **kwargs):
+def save_torch_model(model, filepath, overwrite=True, save_format=None, **kwargs):
     postfix = _TORCH_POSTFIX
 
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
 
-    if not overwrite and osp.isfile(file_path_with_postfix):
-        proceed = ask_to_proceed_with_overwrite(file_path_with_postfix)
+    if not overwrite and osp.isfile(filepath_with_postfix):
+        proceed = ask_to_proceed_with_overwrite(filepath_with_postfix)
         if not proceed:
             return
-        
-    torch.save(model, file_path_with_postfix)
+
+    torch.save(model, filepath_with_postfix)
 
 
-def save_tf_weights(model, file_path, overwrite=True, save_format=None):
+def save_tf_weights(model, filepath, overwrite=True, save_format=None):
     postfix = _TF_POSTFIX
-    
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
+
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
     try:
-        model.save_weights(file_path_with_postfix, overwrite=overwrite, save_format=save_format)
+        model.save_weights(filepath_with_postfix, overwrite=overwrite, save_format=save_format)
     except ValueError as e:
-        model.save_weights(file_path_with_postfix[:-3], overwrite=overwrite, save_format=save_format)
+        model.save_weights(filepath_with_postfix[:-3], overwrite=overwrite, save_format=save_format)
 
 
-def save_torch_weights(model, file_path, overwrite=True, save_format=None):
+def save_torch_weights(model, filepath, overwrite=True, save_format=None):
     postfix = _TORCH_POSTFIX
-  
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
 
-    if not overwrite and osp.isfile(file_path_with_postfix):
-        proceed = ask_to_proceed_with_overwrite(file_path_with_postfix)
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
+
+    if not overwrite and osp.isfile(filepath_with_postfix):
+        proceed = ask_to_proceed_with_overwrite(filepath_with_postfix)
         if not proceed:
             return
 
-    torch.save(model.state_dict(), file_path_with_postfix)     
-        
-    
-def load_tf_weights(model, file_path):
+    torch.save(model.state_dict(), filepath_with_postfix)
+
+
+def load_tf_weights(model, filepath):
     postfix = _TF_POSTFIX
-    
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
+
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
     try:
-        model.load_weights(file_path_with_postfix)
+        model.load_weights(filepath_with_postfix)
     except KeyError as e:
-        model.load_weights(file_path_with_postfix[:-3])
+        model.load_weights(filepath_with_postfix[:-3])
 
 
-def load_torch_weights(model, file_path):
+def load_torch_weights(model, filepath):
     postfix = _TORCH_POSTFIX
-    
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
 
-    checkpoint = torch.load(file_path_with_postfix)
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
+
+    checkpoint = torch.load(filepath_with_postfix)
     model.load_state_dict(checkpoint)
-    
-    
-def load_tf_model(file_path, custom_objects=None, **kwargs):
+
+
+def load_tf_model(filepath, custom_objects=None, **kwargs):
     postfix = _TF_POSTFIX
-    
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
-        
-    return tf.keras.models.load_model(file_path_with_postfix,
+
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
+
+    return tf.keras.models.load_model(filepath_with_postfix,
                                       custom_objects=custom_objects, **kwargs)
 
 
-def load_torch_model(file_path):
+def load_torch_model(filepath):
     postfix = _TORCH_POSTFIX
-   
-    file_path_with_postfix = file_path
-    if not file_path_with_postfix.endswith(postfix):
-        file_path_with_postfix = file_path_with_postfix + postfix
 
-    return torch.load(file_path_with_postfix)    
+    filepath_with_postfix = filepath
+    if not filepath_with_postfix.endswith(postfix):
+        filepath_with_postfix = filepath_with_postfix + postfix
+
+    return torch.load(filepath_with_postfix)
 
 
 def ask_to_proceed_with_overwrite(filepath):
@@ -112,7 +113,7 @@ def ask_to_proceed_with_overwrite(filepath):
       True if we can proceed with overwrite, False otherwise.
     """
     overwrite = six.moves.input('[WARNING] %s already exists - overwrite? '
-                              '[y/n]' % (filepath)).strip().lower()
+                                '[y/n]' % (filepath)).strip().lower()
     while overwrite not in ('y', 'n'):
         overwrite = six.moves.input('Enter "y" (overwrite) or "n" '
                                     '(cancel).').strip().lower()
