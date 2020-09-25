0a2c36bb14413f7a31e3e3a6aebea565e15635e9
EdisonLeeeee <cnljt@outlook.com>
2020-09-25 06:10:54 -0400

    bug fix.

--- graphgallery/transformers/adj_transformer/augment_adj.py
+++ graphgallery/transformers/adj_transformer/augment_adj.py
@@ -6,41 +6,50 @@ from graphgallery.utils.shape import get_length
 from graphgallery.transformers import augment_edge
 
 
-def augment_adj(adj_matrix: sp.csr_matrix, N: int, *,
-                nbrs_per_node: Union[list, np.ndarray, None]=None, 
-                common_nbrs: Union[list, np.ndarray, None]=None,
-                fill_weight: float=1.0) -> sp.csr_matrix:
-    """Augment a specified adjacency matrix by adding N nodes.
+def augment_adj(adj_matrix: sp.csr_matrix, nodes: Union[list, int, np.ndarray], 
+                 edge_weight: np.ndarray=None, *,
+                 nbrs_to_link: Union[list, np.ndarray, None]=None, 
+                 common_nbrs: Union[list, np.ndarray, None]=None,
+                 fill_weight: float=1.0) -> sp.csr_matrix:
+    """Augment a specified adjacency matrix by linking nodes to
+        each element in `nbrs_to_link`.
     
     Examples
     ----------
-    # add 2 nodes, which are adjacent to [2,3] and 3, respectively.
-    >>> augmented_adj = augment_adj(adj_matrix, N=2, 
-                                nbrs_per_node=[[2,3],3], 
+    # add 2 nodes adjacent to [2,3] and 3, respectively.
+    >>> augmented_adj = augment_adj(adj_matrix, nodes=2, 
+                                nbrs_to_link=[[2,3],3], 
                                 fill_weight=1.0)
                                 
                                 
-    # add 2 nodes, all adjacent to [1,2,3].
-    >>> augmented_adj = augment_adj(adj_matrix, N=2, 
+    # add 2 nodes all adjacent to [1,2,3].
+    >>> augmented_adj = augment_adj(adj_matrix, nodes=2, 
                                 common_nbrs=[1,2,3], 
                                 fill_weight=1.0)  
                                 
+     # add 3 edges, [3,1], [4,2], [5,3].
+    >>> augmented_adj = augment_adj(adj_matrix, nodes=[3,4,5], 
+                                common_nbrs=[1,2,3], 
+                                fill_weight=1.0)                                 
     Parameters
     ----------
     adj_matrix: shape [n_nodes, n_nodes].
         A Scipy sparse adjacency matrix.
-    N: number of added nodes.
-        node ids [n_nodes, ..., n_nodes+N-1].
-    nbrs_per_node: shape [N,].
+    nodes: the nodes that will be linked to the graph.
+        list or np.array: the nodes connected to `nbrs_to_link`
+        int: new added nodes connected to `nbrs_to_link`, 
+            node ids [n_nodes, ..., n_nodes+nodes-1].            
+    nbrs_to_link: a list of N elements,
+        where N is the length of 'nodes'.
         the specified neighbor(s) for each added node.
         if `None`, it will be set to `[0, ..., N-1]`.
     common_nbrs: shape [None,].
         specified common neighbors for each added node.
-    fill_weight: edge weight for the added edges.
+    fill_weight: edge weight for the augmented edges.
     
     NOTE:
     ----------
-    Both `nbrs_per_node` and `common_nbrs` should not be specified together.
+    Both `nbrs_to_link` and `common_nbrs` should not be specified together.
     
     
     See Also
@@ -49,31 +58,18 @@ def augment_adj(adj_matrix: sp.csr_matrix, N: int, *,
         
     """
     
-    if nbrs_per_node is not None and common_nbrs is not None:
-        raise RuntimeError("Only one of them should be specified.")
-        
-    if common_nbrs is None:
-        if nbrs_per_node is None:
-            nbrs_per_node = range(N)
-        elif len(nbrs_per_node) != N:
-            raise ValueError("The neighbors of each node should be specified!")
-        
-    n_nodes = adj_matrix.shape[0]
-    n_augmented_nodes = n_nodes + N
-    added_nodes = range(n_nodes, n_augmented_nodes)
-
     adj_matrix = adj_matrix.tocoo(copy=False)
     edge_index = adj_matrix.row, adj_matrix.col
 
-    augmented_edge_index, augmented_edge_weight = augment_edge(edge_index, N,
+    augmented_edge_index, augmented_edge_weight = augment_edge(edge_index, nodes,
                                                                edge_weight=adj_matrix.data,
-                                                               nbrs_per_node=nbrs_per_node,
+                                                               nbrs_to_link=nbrs_to_link,
                                                                common_nbrs=common_nbrs,
                                                                fill_weight=fill_weight)
     
-    
+    N = augmented_edge_index.max() + 1
     augmented_adj = sp.csr_matrix((augmented_edge_weight, augmented_edge_index), 
-                               shape=(n_augmented_nodes, n_augmented_nodes))
+                               shape=(N, N))
     
     augmented_adj.eliminate_zeros()
     return augmented_adj
--- graphgallery/transformers/edge_transformer/augment_edge.py
+++ graphgallery/transformers/edge_transformer/augment_edge.py
@@ -4,32 +4,38 @@ import scipy.sparse as sp
 from typing import Union
 from graphgallery.utils.shape import get_length
 from graphgallery.transformers import edge_transpose
+from graphgallery import is_interger_scalar
 
-def augment_edge(edge_index: np.ndarray, N: int, edge_weight: np.ndarray=None, *,
-                nbrs_per_node: Union[list, np.ndarray, None]=None, 
-                common_nbrs: Union[list, np.ndarray, None]=None,
-                fill_weight: float=1.0) -> sp.csr_matrix:
-    """Augment a set of edges by adding N nodes.
+def augment_edge(edge_index: np.ndarray, nodes: Union[list, int, np.ndarray], 
+                 edge_weight: np.ndarray=None, *,
+                 nbrs_to_link: Union[list, np.ndarray, None]=None, 
+                 common_nbrs: Union[list, np.ndarray, None]=None,
+                 fill_weight: float=1.0) -> tuple:
+    """Augment a set of edges by linking nodes to
+        each element in `nbrs_to_link`.
     
                                 
     Parameters
     ----------
     edge_index: shape [M, 2] or [2, M] -> [2, M]
             edge indices of a Scipy sparse adjacency matrix.
-    N: number of added nodes.
-        node ids [n_nodes, ..., n_nodes+N-1].            
+    nodes: the nodes that will be linked to the graph.
+        list or np.array: the nodes connected to `nbrs_to_link`
+        int: new added nodes connected to `nbrs_to_link`, 
+            node ids [n_nodes, ..., n_nodes+nodes-1].            
     edge_weight: shape [M,]
-            edge weights of a Scipy sparse adjacency matrix.
-    nbrs_per_node: shape [N,].
+        edge weights of a Scipy sparse adjacency matrix.
+    nbrs_to_link: a list of N elements,
+        where N is the length of 'nodes'.
         the specified neighbor(s) for each added node.
         if `None`, it will be set to `[0, ..., N-1]`.
     common_nbrs: shape [None,].
         specified common neighbors for each added node.
-    fill_weight: edge weight for the added edges.
+    fill_weight: edge weight for the augmented edges.
     
     NOTE:
     ----------
-    Both `nbrs_per_node` and `common_nbrs` should not be specified together.
+    Both `nbrs_to_link` and `common_nbrs` should not be specified together.
     
     See Also
     ----------
@@ -37,37 +43,41 @@ def augment_edge(edge_index: np.ndarray, N: int, edge_weight: np.ndarray=None, *
         
     """
     
-    if nbrs_per_node is not None and common_nbrs is not None:
+    if nbrs_to_link is not None and common_nbrs is not None:
         raise RuntimeError("Only one of them should be specified.")
         
-    if common_nbrs is None:
-        if nbrs_per_node is None:
-            nbrs_per_node = range(N)
-        elif len(nbrs_per_node) != N:
-            raise ValueError("The neighbors of each node should be specified!")
-            
     edge_index = edge_transpose(edge_index)
     
     if edge_weight is None:
         edge_weight = np.ones(edge_index.shape[1], dtype=floatx())    
-
+        
     n_nodes = edge_index.max() + 1
-    n_augmented_nodes = n_nodes + N
-    added_nodes = range(n_nodes, n_augmented_nodes)
+    
+    if is_interger_scalar(nodes):
+        # int, add nodes to the graph
+        nodes = np.arange(n_nodes, n_nodes + nodes, dtype=edge_index.dtype)
+    else:
+        # array-like, link nodes to the graph
+        nodes = np.asarray(nodes, dtype=edge_index.dtype)
 
-    if nbrs_per_node is not None:
-        added_edges = np.vstack([added_nodes, nbrs_per_node])
-        added_edges = np.hstack([np.vstack([np.tile(node, get_length(nbr)), nbr]) 
-                                            for node, nbr in zip(added_nodes, nbrs_per_node)]) 
+    if common_nbrs is None and nbrs_to_link is None:
+        nbrs_to_link = np.arange(nodes.size, dtype=edge_index.dtype)
+    
+    if not nodes.size == len(nbrs_to_link):
+        raise ValueError("The length of 'nbrs_to_link' should equal to 'nodes'.")
+
+    if nbrs_to_link is not None:
+        edges_to_link = np.hstack([np.vstack([np.tile(node, get_length(nbr)), nbr]) 
+                                            for node, nbr in zip(nodes, nbrs_to_link)]) 
     else:
         n_repeat = len(common_nbrs)
-        added_edges = np.hstack([np.vstack([np.tile(node, n_repeat), common_nbrs]) 
-                                 for node in added_nodes])
+        edges_to_link = np.hstack([np.vstack([np.tile(node, n_repeat), common_nbrs]) 
+                                 for node in nodes])
         
-    added_edges_T = added_edges[[1,0]]
-    added_edge_weight = np.zeros(added_edges.shape[1]*2, dtype=edge_weight.dtype) + fill_weight
+    edges_to_link = np.hstack([edges_to_link, edges_to_link[[1,0]]])
+    added_edge_weight = np.zeros(edges_to_link.shape[1], dtype=edge_weight.dtype) + fill_weight
 
-    augmented_edge_index = np.hstack([edge_index, added_edges, added_edges_T])
+    augmented_edge_index = np.hstack([edge_index, edges_to_link])
     augmented_edge_weight = np.hstack([edge_weight, added_edge_weight])
     
     return augmented_edge_index, augmented_edge_weight
