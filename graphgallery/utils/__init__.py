63550faeaed182fc34e639616ebd70d43973be9d
EdisonLeeeee <cnljt@outlook.com>
2020-10-03 01:12:32 +0800

    fix

--- graphgallery/utils/__init__.py
+++ graphgallery/utils/__init__.py
@@ -1,4 +1,2 @@
-from graphgallery.utils.history import History
 from graphgallery.utils.decorators import EqualVarLength, get_length
-from graphgallery.utils.probar import progress_bar
 from graphgallery.utils.bunch import Bunch
