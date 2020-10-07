9ba284daf066b015dab9bb2d1c217be30d3d371f
Aria461863631 <461863631@qq.com>
2020-10-07 14:31:36 +0000

    add test utils/device

--- test/utils/test_shape.py
+++ test/utils/test_shape.py
@@ -3,8 +3,10 @@ from graphgallery.utils.shape import repeat
 
 def test_repeat():
     assert repeat(None, length=4) == [None, None, None, None]
-    assert repeat(4, length=4) == [4, 4, 4, 4]
     assert repeat([], length=4) == []
+    assert repeat((), length=4) == []
+    assert repeat(4, length=4) == [4, 4, 4, 4]
+    assert repeat("check", length=3) == ["check", "check", "check"]
     assert repeat([2, 3, 4], length=4) == [2, 3, 4, 4]
     assert repeat([1, 2, 3, 4], length=4) == [1, 2, 3, 4]
     assert repeat([1, 2, 3, 4, 5], length=4) == [1, 2, 3, 4]
