e43985b7a3ce6b047c1964fcd3643910019490d3
Aria461863631 <461863631@qq.com>
2020-12-02 03:34:10 +0000

    modify examples

---
+++ examples/TensorFlow/GAT.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import graphgallery 
+import tensorflow as tf
+
+graphgallery.set_memory_growth()
+
+print("GraphGallery version: ", graphgallery.__version__)
+print("TensorFlow version: ", tf.__version__)
+
+'''
+Load Datasets
+- cora/citeseer/pubmed
+'''
+from graphgallery.datasets import Planetoid
+data = Planetoid('cora', root="~/GraphData/datasets/", verbose=False)
+graph = data.graph
+splits = data.split_nodes()
+
+from graphgallery.gallery import GAT
+model = GAT(graph, attr_transform="normalize_attr", device="gpu", seed=123)
+model.build()
+his = model.train(splits.train_nodes, splits.val_nodes, verbose=1, epochs=100)
+results = model.test(splits.test_nodes) 
+print(f'Test loss {results.loss:.5}, Test accuracy {results.accuracy:.2%}')
