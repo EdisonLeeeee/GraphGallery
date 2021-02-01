826d26959a5160bbfe7fd1c8719fd8ed46405d3e
EdisonLeeeee <cnljt@outlook.com>
2021-02-01 01:42:06 -0500

    update

--- setup.py
+++ setup.py
@@ -22,7 +22,13 @@ def get_version():
 VERSION = get_version()
 url = 'https://github.com/EdisonLeeeee/GraphGallery'
 
+
 install_requires = [
+    # Do not add pytorch and tensorflow here. User should install
+    # pytorch and tensorflow themselves, preferrably by OS's package manager, or by
+    # choosing the proper pypi package name at
+    # pytorch: https://pytorch.org/
+    # tensorflow: http://tensorflow.org/    
     'tqdm',
     'yacs',
     'scipy',
@@ -30,8 +36,6 @@ install_requires = [
     'tabulate',
     'pandas',
     'scikit_learn',
-    'torch>=1.4',
-    'tensorflow>=2.1.0',
     'networkx>=2.3',
     'gensim>=3.8.0',
     'numba>=0.46.0',
@@ -45,7 +49,7 @@ setup(
     version=VERSION,
     description='A Gallery for Benchmarking Graph Neural Networks and Graph Adversarial Learning.',
     author='Jintang Li',
-    author_email='cnljt@outlook.com',
+    author_email='lijt55@mail2.sysu.edu.cn',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url=url,
@@ -66,12 +70,16 @@ setup(
     packages=find_packages(exclude=("examples", "imgs", "benchmark", "test")),
     classifiers=[
         'Development Status :: 3 - Alpha',
-        "Programming Language :: Python :: 3",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Libraries',
-        "Operating System :: OS Independent"
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",        
     ],
 )
