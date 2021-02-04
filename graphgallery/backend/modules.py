9f8ee43e1777877527658a3ddf76bbd962f506c7
EdisonLeeeee <cnljt@outlook.com>
2021-02-04 16:57:50 +0800

    fix

--- graphgallery/backend/modules.py
+++ graphgallery/backend/modules.py
@@ -1,10 +1,10 @@
 import torch
 import tensorflow
-import numpy
 
 
 __all__ = ['BackendModule', 'TensorFlowBackend',
-           'PyTorchBackend', 'PyGBackend', 'DGLPyTorchBackend', 'DGLTensorFlowBackend']
+           'PyTorchBackend', 'PyGBackend',
+           'DGLPyTorchBackend', 'DGLTensorFlowBackend']
 
 
 class BackendModule:
@@ -16,21 +16,21 @@ class BackendModule:
         self.acceptable_names = self.alias
 
     @property
-    def version(self) -> str:
+    def version(self):
         raise NotImplementedError
 
     @property
-    def name(self) -> str:
+    def name(self):
         raise NotImplementedError
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return NotImplementedError
 
     def __eq__(self, value) -> bool:
         return str(value).lower() in self.acceptable_names
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"{self.name} {self.extra_repr()} Backend"
     __str__ = __repr__
 
@@ -46,15 +46,15 @@ class PyTorchBackend(BackendModule):
         self.acceptable_names = self.acceptable_names.union({"pth", "th", "torch", "pytorch"})
 
     @property
-    def version(self) -> str:
+    def version(self):
         return torch.__version__
 
     @property
-    def name(self) -> str:
+    def name(self):
         return "PyTorch"
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return "pytorch"
 
 
@@ -66,15 +66,15 @@ class TensorFlowBackend(BackendModule):
         self.acceptable_names = self.acceptable_names.union({"tf", "tensorflow"})
 
     @property
-    def version(self) -> str:
+    def version(self):
         return tensorflow.__version__
 
     @property
-    def name(self) -> str:
+    def name(self):
         return "TensorFlow"
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return "tensorflow"
 
 
@@ -86,16 +86,16 @@ class PyGBackend(PyTorchBackend):
         self.acceptable_names = self.acceptable_names.union({"pyg"})
 
     @property
-    def version(self) -> str:
+    def version(self):
         import torch_geometric
         return torch_geometric.__version__
 
     @property
-    def name(self) -> str:
+    def name(self):
         return "PyTorch Geometric"
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return "pyg"
 
     def extra_repr(self):
@@ -111,16 +111,16 @@ class DGLTensorFlowBackend(TensorFlowBackend):
         self.acceptable_names = self.acceptable_names.union({"dgl_tf"})
 
     @property
-    def version(self) -> str:
+    def version(self):
         import dgl
         return dgl.__version__
 
     @property
-    def name(self) -> str:
+    def name(self):
         return "DGL TensorFlow"
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return "dgl_tf"
 
     def extra_repr(self):
@@ -135,16 +135,16 @@ class DGLPyTorchBackend(PyTorchBackend):
         self.acceptable_names = self.acceptable_names.union({"dgl_torch", "dgl_th", "dgl"})
 
     @property
-    def version(self) -> str:
+    def version(self):
         import dgl
         return dgl.__version__
 
     @property
-    def name(self) -> str:
+    def name(self):
         return "DGL PyTorch"
 
     @property
-    def abbr(self) -> str:
+    def abbr(self):
         return "dgl_torch"
 
     def extra_repr(self):
