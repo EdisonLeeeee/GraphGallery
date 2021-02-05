ee92f829da6b70c99142d136f0b33b199e38f6d3
EdisonLeeeee <cnljt@outlook.com>
2021-02-05 16:09:47 +0800

    add MUSAE test

--- test/datasets/test_datasets.py
+++ test/datasets/test_datasets.py
@@ -6,52 +6,60 @@ def test_Planetoid():
         data = dataset(d, verbose=None)
         graph = data.graph
         splits = data.split_nodes()
-        
+
+
 def test_NPZDataset():
     from graphgallery.datasets import NPZDataset
     dataset = NPZDataset
     for d in dataset.available_datasets():
         data = dataset(d, verbose=None)
         graph = data.graph
-        
+
+
 def test_KarateClub():
     from graphgallery.datasets import KarateClub
     dataset = KarateClub
     for d in dataset.available_datasets():
         data = dataset(d, verbose=None)
         graph = data.graph
-        
+
+
 def test_MUSAE():
     from graphgallery.datasets import MUSAE
     dataset = MUSAE
     for d in dataset.available_datasets():
         data = dataset(d, verbose=None)
         graph = data.graph
-        
+
     # attenuated
     for d in dataset.available_datasets():
         data = dataset(d, verbose=None, attenuated=True)
-        graph = data.graph        
-        
+        graph = data.graph
+
+
 def test_PPI():
     from graphgallery.datasets import PPI
     data = PPI(verbose=True)
     graph = data.graph
-    
+
+
 def test_Reddit():
     from graphgallery.datasets import Reddit
     data = Reddit(verbose=True)
     graph = data.graph
-    
+
+
 def test_TUDataset():
     from graphgallery.datasets import TUDataset
-    data = TUDataset("COLLAB", verbose=True)   
+    data = TUDataset("COLLAB", verbose=True)
     graph = data.graph
-    
+
+
 if __name__ == "__main__":
     test_Planetoid()
     test_NPZDataset()
     test_KarateClub()
+    test_MUSAE()
     test_PPI()
     test_Reddit()
     test_TUDataset()
