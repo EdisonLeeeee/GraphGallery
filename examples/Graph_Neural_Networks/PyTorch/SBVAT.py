66f57c88648e2cba3dd4e98528ea88899d9bb1be
EdisonLeeeee <cnljt@outlook.com>
2021-05-10 10:27:32 +0800

    add examples

--- examples/Graph_Neural_Networks/PyTorch/SBVAT.py
+++ examples/Graph_Neural_Networks/PyTorch/SBVAT.py
@@ -18,8 +18,8 @@ splits = data.split_nodes()
 
 graphgallery.set_backend("pytorch")
 
-from graphgallery.gallery.nodeclas import GCN_VAT
-trainer = GCN_VAT(device="gpu", seed=123).make_data(graph, attr_transform="normalize_attr").build()
-his = trainer.fit(splits.train_nodes, splits.val_nodes, verbose=1, epochs=100)
+from graphgallery.gallery.nodeclas import SBVAT
+trainer = SBVAT(device="gpu", seed=123).make_data(graph, attr_transform="normalize_attr").build()
+his = trainer.fit(splits.train_nodes, splits.val_nodes, verbose=1, epochs=200)
 results = trainer.evaluate(splits.test_nodes)
 print(f'Test loss {results.loss:.5}, Test accuracy {results.accuracy:.2%}')
