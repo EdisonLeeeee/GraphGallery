e41caeb32a07da95364f15b85cad527a67763255
EdisonLeeeee <cnljt@outlook.com>
2021-07-15 11:07:39 +0800

    fix numba error

--- graphgallery/attack/targeted/common/nettack.py
+++ graphgallery/attack/targeted/common/nettack.py
@@ -18,9 +18,10 @@ from graphgallery import functional as gf
 from graphgallery.attack.targeted import Common
 from graphgallery.utils import tqdm
 
-from numba.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
-warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
-warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
+# this may cause errors
+# from numba.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
+# warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+# warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
 
 from ..targeted_attacker import TargetedAttacker
 
