84ecee211af6fb9bcbf4c580d722157ed58b9bf9
EdisonLeeeee <cnljt@outlook.com>
2021-09-12 16:36:30 +0800

    add get_file from tf.keras.utils.get_file

--- graphgallery/data/io.py
+++ graphgallery/data/io.py
@@ -4,16 +4,20 @@ import tarfile
 import errno
 import json
 import zipfile
+import shutil
 import os.path as osp
 import numpy as np
 import pandas as pd
 
-from tensorflow.keras.utils import get_file
+from six.moves.urllib.error import HTTPError
+from six.moves.urllib.error import URLError
+from six.moves.urllib.request import urlretrieve
 
+from graphgallery.utils import Progbar
 
 __all__ = [
     'download_file', 'files_exist', 'makedirs', 'makedirs_from_filepath',
-    'extractall', 'remove', 'load_npz', 'read_csv', 'read_json',
+    'extractall', 'remove', 'load_npz', 'read_csv', 'read_json', 'get_file',
 ]
 
 
@@ -38,33 +42,169 @@ def download_file(raw_paths, urls):
         raise exceptions[0]
 
 
-def extractall(filename, folder=None):
-    """Extracts a zip or tar.gz (tgz) archive to a specific folder.
+def extractall(filename, folder=None, archive_format='auto'):
+    """Extracts an archive if it matches tar, tar.gz, tar.bz, or zip formats.
 
     Parameters:
     -----------
-    filename (string): The path to the tar archive.
-    folder (string): The folder.
+    filename: path to the archive file, which could be a list of paths.
+    folder: folder to extract the archive file
+    archive_format: Archive format to try for extracting the file.
+        Options are 'auto', 'tar', 'zip', and None.
+        'tar' includes tar, tar.gz, and tar.bz files.
+        The default 'auto' is ['tar', 'zip'].
+        None or an empty list will return no matches found.
+
+    Returns:
+    -----------
+    True if a match was found and an archive extraction was completed,
+        False otherwise.
     """
+
     if not filename:
-        return
+        return False
 
     if isinstance(filename, (list, tuple)):
         for f in filename:
             extractall(f, folder)
-        return
+        return True
 
     if folder is None:
         folder = osp.dirname(osp.realpath(osp.expanduser(filename)))
 
-    if filename.endswith(".zip"):
-        with zipfile.ZipFile(filename, 'r') as f:
-            f.extractall(folder)
+    if archive_format is None:
+        return False
+    if archive_format == 'auto':
+        archive_format = ['tar', 'zip']
+    if isinstance(archive_format, str):
+        archive_format = [archive_format]
 
-    if filename.endswith(".tgz") or filename.endswith(".tar.gz"):
-        tar = tarfile.open(filename, "r:gz")
-        tar.extractall(path=folder)
-        tar.close()
+    for archive_type in archive_format:
+        if archive_type == 'tar':
+            open_fn = tarfile.open
+            is_match_fn = tarfile.is_tarfile
+        elif archive_type == 'zip':
+            open_fn = zipfile.ZipFile
+            is_match_fn = zipfile.is_zipfile
+        else:
+            raise ValueError(archive_type)
+
+        if is_match_fn(filename):
+            with open_fn(filename) as archive:
+                try:
+                    archive.extractall(folder)
+                except (tarfile.TarError, RuntimeError, KeyboardInterrupt):
+                    if osp.exists(folder):
+                        if osp.isfile(folder):
+                            os.remove(folder)
+                        else:
+                            shutil.rmtree(folder)
+                    raise
+            return True
+    return False
+
+
+def get_file(fname,
+             origin,
+             untar=False,
+             cache_subdir='datasets',
+             extract=False,
+             archive_format='auto',
+             cache_dir=None):
+    """Downloads a file from a URL if it not already in the cache.
+
+    By default the file at the url `origin` is downloaded to the
+    cache_dir `~/.graphgallery`, placed in the cache_subdir `datasets`,
+    and given the filename `fname`. The final location of a file
+    `example.txt` would therefore be `~/.graphgallery/datasets/example.txt`.
+
+    Files in tar, tar.gz, tar.bz, and zip formats can also be extracted.
+    Passing a hash will verify the file after download. The command line
+    programs `shasum` and `sha256sum` can compute the hash.
+
+    Note:
+    -----------
+    This function was initially written by TensorFlow Authors. See
+    `tf.keras.utils.get_file`. Copyright The TensorFlow Authors. All Rights Reserved.
+
+
+    Parameters:
+    -----------
+    fname: Name of the file. If an absolute path `/path/to/file.txt` is
+        specified the file will be saved at that location.
+    origin: Original URL of the file.
+    untar: Deprecated in favor of 'extract'.
+        boolean, whether the file should be decompressed
+    cache_subdir: Subdirectory under the GraphGallery cache dir where the file is
+        saved. If an absolute path `/path/to/folder` is
+        specified the file will be saved at that location.
+    extract: True tries extracting the file as an Archive, like tar or zip.
+    archive_format: Archive format to try for extracting the file.
+        Options are 'auto', 'tar', 'zip', and None.
+        'tar' includes tar, tar.gz, and tar.bz files.
+        The default 'auto' is ['tar', 'zip'].
+        None or an empty list will return no matches found.
+    cache_dir: Location to store cached files, when None it
+        defaults to the [GraphGallery Directory]
+
+    Returns:
+    -----------
+    Path to the downloaded file
+    """
+    if cache_dir is None:
+        cache_dir = os.path.join(os.path.expanduser('~'), '.graphgallery')
+
+    datadir_base = os.path.expanduser(cache_dir)
+    if not os.access(datadir_base, os.W_OK):
+        datadir_base = os.path.join('/tmp', '.graphgallery')
+    datadir = os.path.join(datadir_base, cache_subdir)
+    makedirs(datadir)
+
+    if untar:
+        untar_fpath = os.path.join(datadir, fname)
+        fpath = untar_fpath + '.tar.gz'
+    else:
+        fpath = os.path.join(datadir, fname)
+
+    if not os.path.exists(fpath):
+        print('Downloading data from', origin)
+
+        class ProgressTracker(object):
+            # Maintain progbar for the lifetime of download.
+            # This design was chosen for Python 2.7 compatibility.
+            progbar = None
+
+        def dl_progress(count, block_size, total_size):
+            if ProgressTracker.progbar is None:
+                if total_size == -1:
+                    total_size = None
+                ProgressTracker.progbar = Progbar(total_size)
+            else:
+                ProgressTracker.progbar.update(count * block_size)
+
+        error_msg = 'URL fetch failure on {}: {} -- {}'
+        try:
+            try:
+                urlretrieve(origin, fpath, dl_progress)
+            except HTTPError as e:
+                raise Exception(error_msg.format(origin, e.code, e.msg))
+            except URLError as e:
+                raise Exception(error_msg.format(origin, e.errno, e.reason))
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(fpath):
+                os.remove(fpath)
+            raise
+        ProgressTracker.progbar = None
+
+    if untar:
+        if not os.path.exists(untar_fpath):
+            extractall(fpath, datadir, archive_format='tar')
+        return untar_fpath
+
+    if extract:
+        extractall(fpath, datadir, archive_format)
+
+    return fpath
 
 
 def remove(filepaths):
@@ -84,11 +224,11 @@ def files_exist(files) -> bool:
         return osp.exists(files)
 
 
-def makedirs(path: str):
+def makedirs(folder: str):
     try:
-        os.makedirs(osp.expanduser(osp.normpath(path)), exist_ok=True)
+        os.makedirs(osp.expanduser(osp.normpath(folder)), exist_ok=True)
     except OSError as e:
-        if e.errno != errno.EEXIST and osp.isdir(path):
+        if e.errno != errno.EEXIST and osp.isdir(folder):
             raise e
 
 
@@ -113,14 +253,14 @@ def load_npz(filepath):
         raise ValueError(f"{filepath} doesn't exist.")
 
 
-def read_csv(reader, dtype=np.int32):
+def read_csv(reader, dtype=np.int32, sep=","):
     if isinstance(reader, str):
         reader = osp.abspath(osp.expanduser(reader))
     else:
         reader = io.BytesIO(reader)
     return pd.read_csv(reader,
                        encoding="utf8",
-                       sep=",",
+                       sep=sep,
                        dtype={"switch": dtype})
 
 
