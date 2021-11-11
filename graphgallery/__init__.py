c37f66e3455963265161a5f6d78c36cae7a730b1
EdisonLeeeee <cnljt@outlook.com>
2021-11-11 11:16:39 +0800

    temp

--- graphgallery/__init__.py
+++ graphgallery/__init__.py
@@ -2,9 +2,9 @@ from graphgallery.backend import *
 from graphgallery.data_type import *
 
 # modules
-from graphgallery import nn
 from graphgallery import backend
-from graphgallery import gallery
+# from graphgallery import nn
+# from graphgallery import gallery
 from graphgallery import utils
 from graphgallery import data
 from graphgallery import datasets
